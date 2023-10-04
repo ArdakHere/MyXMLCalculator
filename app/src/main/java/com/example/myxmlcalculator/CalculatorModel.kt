57aab97d858915db9d99b3ef121e25bbf24e4383
ArdakHere <mrglok1@gmail.com>
2023-10-04 19:47:18 +0600

    Added the multi-digit multi-operator calculation. Although, the calculations do not work as intended when decimal points (.) are included. Have to fix that. Also have to add parentheses support in the future.

--- app/src/main/java/com/example/myxmlcalculator/CalculatorModel.kt
+++ app/src/main/java/com/example/myxmlcalculator/CalculatorModel.kt
@@ -1,5 +1,6 @@
 package com.example.myxmlcalculator
 
+import java.lang.StringBuilder
 import java.util.Stack
 
 class CalculatorModel {
@@ -12,14 +13,14 @@ class CalculatorModel {
 
     fun evaluateRPN(expression: String): String{
 
-        val stack = Stack<Double>()
+        val stack = Stack<Int>()
         val operators = setOf("+", "-", "x", "/")
 
         val tokens = expression.split(Regex("\\s+"))
 
         for (token in tokens) {
             if (!operators.contains(token)) {
-                stack.push(token.toDouble())
+                stack.push(token.toInt())
             } else {
                 val operand2 = stack.pop()
                 val operand1 = stack.pop()
@@ -35,6 +36,72 @@ class CalculatorModel {
     }
     fun inputToRPN(infixExpression: String): String {
         val stack = ArrayDeque<Char>()
+        val output = mutableListOf<String>() // Use a list of strings instead of characters
+        val precedence = mapOf('+' to 1, '-' to 1, 'x' to 2, '/' to 2)
+        var i = 0
+        //Dekspan plus
+        var exitLoopMarker = 0
+        var multiDigitNumber = StringBuilder()
+
+        while (i<infixExpression.length) {
+            when {
+                infixExpression[i].isDigit() -> {
+                    while(infixExpression[i].isDigit()) {
+                        multiDigitNumber.append(infixExpression[i])
+                        if(i+1 == infixExpression.length){
+                            exitLoopMarker = 1
+                            i++
+                            break
+
+                        } else {
+                            i++
+                        }
+                    }
+                    output.add(multiDigitNumber.toString()) // Add digits as separate
+                    // strings
+                    multiDigitNumber.clear()
+                    continue
+                }
+
+                infixExpression[i] in "+-x/" -> {
+                    while (stack.isNotEmpty() && stack.first() in "+-x/" &&
+                        precedence[infixExpression[i]]!! <= precedence[stack.first()]!!) {
+                        output.add(stack.removeFirst().toString()) // Add operators as separate strings
+                    }
+                    stack.addFirst(infixExpression[i])
+                }
+                infixExpression[i] == '(' -> {
+                    stack.addFirst(infixExpression[i])
+                }
+                infixExpression[i] == ')' -> {
+                    while (stack.isNotEmpty() && stack.first() != '(') {
+                        output.add(stack.removeFirst().toString()) // Add operators as separate strings
+                    }
+                    if (stack.isNotEmpty() && stack.first() == '(') {
+                        stack.removeFirst() // Remove the opening parenthesis
+                    }
+                }
+                infixExpression[i].isWhitespace() -> {
+                    // Ignore whitespace
+                }
+                else -> {
+                    // Handle other characters (variables, functions, etc.) if needed
+                }
+            }
+            i++
+        }
+
+        while (stack.isNotEmpty()) {
+            output.add(stack.removeFirst().toString()) // Add remaining operators as separate strings
+        }
+
+        return output.joinToString(" ") // Join with spaces
+    }
+}
+
+
+/*
+val stack = ArrayDeque<Char>()
         val output = mutableListOf<String>() // Use a list of strings instead of characters
         val precedence = mapOf('+' to 1, '-' to 1, 'x' to 2, '/' to 2)
 
@@ -77,7 +144,10 @@ class CalculatorModel {
 
         return output.joinToString(" ") // Join with spaces
     }
-}
+ */
+
+
+
 
 
 /*
